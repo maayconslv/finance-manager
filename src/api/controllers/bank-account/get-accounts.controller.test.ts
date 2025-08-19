e6bc0c93b1722ecb9eb7f4ddaf3ca36365eefd20
mayconslv <mayconslv.contato@gmail.com>
2025-08-19 02:17:12 -0300

    feat: testing use case and controller of get user accounts

---
+++ src/api/controllers/bank-account/get-accounts.controller.test.ts
@@ -0,0 +1,108 @@
+import { Money, UniqueEntityId } from "@/core/object-values";
+import { UserAccountsModel } from "@/domain/Accounts/application/model";
+import { BankAccountEntity } from "@/domain/Accounts/enterprise";
+import { UserEntity, WalletEntity } from "@/domain/Auth/enterprise/entities";
+import { RequestMaker, TestServer } from "@/test";
+import { createBankAccount, createUser, createWallet } from "@/test/seed.test";
+import { PrismaClient } from "@prisma/client";
+import { expect } from "chai";
+import jwt from "jsonwebtoken";
+
+describe("Controller - Get user accounts - GET", () => {
+  let testServer: TestServer;
+  let prismaRepository: PrismaClient;
+  let requestMaker: RequestMaker;
+  let userData: UserEntity;
+  let walletData: WalletEntity;
+  let bankAccountsData: BankAccountEntity[];
+  let token: string;
+
+  before(() => {
+    requestMaker = new RequestMaker();
+    testServer = new TestServer();
+    prismaRepository = new PrismaClient();
+    testServer.start();
+  });
+
+  beforeEach(async () => {
+    userData = await createUser();
+    walletData = await createWallet({ userId: userData.id });
+    bankAccountsData = [
+      createBankAccount({ walletId: new UniqueEntityId(walletData.id) }),
+      createBankAccount({ walletId: new UniqueEntityId(walletData.id) }),
+    ];
+
+    await prismaRepository.user.create({
+      data: {
+        id: userData.id.toString(),
+        name: userData.name,
+        email: userData.email.toString(),
+        passwordHash: userData.password,
+        salt: userData.salt,
+      },
+    });
+    await prismaRepository.wallet.create({
+      data: {
+        id: walletData.id.toString(),
+        currentBalance: walletData.currentBalance.getInCents(),
+        initialBalance: walletData.initialBalance.getInCents(),
+        userId: userData.id.toString(),
+      },
+    });
+    await prismaRepository.bankAccount.createMany({
+      data: bankAccountsData.map((item) => ({
+        id: item.id,
+        walletId: item.walletId,
+        bankName: item.bankName,
+        accountName: item.accountName,
+        initialBalance: item.initialBalance.getInCents(),
+        currentBalance: item.currentBalance.getInCents(),
+        createdAt: item.createdAt,
+      })),
+    });
+
+    token = jwt.sign({ userId: userData.id }, process.env["JWT_SECRET"]!);
+  });
+
+  afterEach(async () => {
+    await prismaRepository.bankAccount.deleteMany();
+    await prismaRepository.wallet.deleteMany();
+    await prismaRepository.user.deleteMany();
+  });
+
+  after(() => {
+    testServer.stop();
+  });
+
+  it("should be able to get users accounts correctly", async () => {
+    const response = await requestMaker.execute<UserAccountsModel>({
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      path: "/accounts",
+    });
+
+    const bankTotalAmount = bankAccountsData.reduce((acc, account) => acc + account.currentBalance.getInCents(), 0);
+    const walletTotalAmount = walletData.currentBalance.getInCents();
+    const totalAmountData = Money.fromCents(bankTotalAmount + walletTotalAmount);
+    expect(totalAmountData.toBRL()).to.be.equal(response.body.data.totalAmount);
+    expect(response.body.data.wallet.id).to.be.equal(walletData.id);
+    expect(response.body.data.wallet.currentBalance).to.be.equal(walletData.currentBalance.toBRL());
+    expect(response.body.data.wallet.userId).to.be.equal(walletData.userId);
+  });
+
+  it("should return unauthorized error if the user is using a invalid token", async () => {
+    const response = await requestMaker.execute<UserAccountsModel>({
+      method: "get",
+      headers: {
+        Authorization: `Bearer invalid-token`,
+      },
+      path: "/accounts",
+    });
+
+    expect(response.body.errors.message).to.be.equal("Invalid token");
+    expect(response.body.errors.errorType).to.be.equal("UNAUTHORIZED");
+    expect(response.body.errors.statusCode).to.be.equal(401);
+  });
+});
--- src/domain/Accounts/application/use-cases/get-accounts.use-case.test.ts
+++ src/domain/Accounts/application/use-cases/get-accounts.use-case.test.ts
@@ -3,7 +3,11 @@ import Container, { ContainerInstance } from "typedi";
 import { GetAccountsUseCase } from "./get-accounts.use-case";
 import { BankAccountEntity } from "../../enterprise";
 import { BankAccountRepository, UserRepository, WalletRepository } from "@/infrastructure/database/prisma";
-import { BankAccountInMemoryRepository, UserInMemoryRepository, WalletInMemoryRepository } from "@/infrastructure/database/in-memory";
+import {
+  BankAccountInMemoryRepository,
+  UserInMemoryRepository,
+  WalletInMemoryRepository,
+} from "@/infrastructure/database/in-memory";
 import { createBankAccount, createUser, createWallet } from "@/test/seed.test";
 import { Money, UniqueEntityId } from "@/core/object-values";
 import { expect } from "chai";
@@ -17,7 +21,7 @@ describe("Application - Get accounts - Use case", () => {
 
   const inMemoryUsers: UserEntity[] = [];
   const inMemoryWallets: WalletEntity[] = [];
-  let inMemoryBankAccounts: BankAccountEntity[] = [];
+  const inMemoryBankAccounts: BankAccountEntity[] = [];
 
   before(() => {
     testContainer = Container.of("test-container");
@@ -37,7 +41,7 @@ describe("Application - Get accounts - Use case", () => {
       createBankAccount({ walletId: new UniqueEntityId(walletData.id) }),
       createBankAccount({ walletId: new UniqueEntityId(walletData.id) }),
       createBankAccount({ walletId: new UniqueEntityId(walletData.id) }),
-    ]
+    ];
 
     inMemoryBankAccounts.push(...bankAccountsData);
     inMemoryWallets.push(walletData);
@@ -48,48 +52,48 @@ describe("Application - Get accounts - Use case", () => {
     inMemoryBankAccounts.length = 0;
     inMemoryUsers.length = 0;
     inMemoryWallets.length = 0;
-  })
+  });
 
   it("should be able to get all user accounts correctly", async () => {
-    const { bankAccounts, totalAmount, wallet } = await sut.execute({ userId: user.id })
+    const { bankAccounts, totalAmount, wallet } = await sut.execute({ userId: user.id });
 
-    const bankTotalAmount = bankAccountsData.reduce((acc, account) => acc + account.currentBalance.getInCents(), 0)
+    const bankTotalAmount = bankAccountsData.reduce((acc, account) => acc + account.currentBalance.getInCents(), 0);
     const walletTotalAmount = walletData.currentBalance.getInCents();
     const totalAmountData = Money.fromCents(bankTotalAmount + walletTotalAmount);
     expect(totalAmountData.toBRL()).to.be.equal(totalAmount);
     expect(bankAccounts).to.have.lengthOf(inMemoryBankAccounts.length);
     bankAccounts.forEach((item, index) => {
-      expect(item.id).to.be.equal(bankAccounts[index]!.id);
-      expect(item.accountName).to.be.equal(bankAccounts[index]!.accountName);
-      expect(item.bankName).to.be.equal(bankAccounts[index]!.bankName);
-      expect(item.currentBalance).to.be.equal(bankAccounts[index]!.currentBalance);
-      expect(item.initialBalance).to.be.equal(bankAccounts[index]!.initialBalance);
+      expect(item.id).to.be.equal(bankAccountsData[index]!.id);
+      expect(item.accountName).to.be.equal(bankAccountsData[index]!.accountName);
+      expect(item.bankName).to.be.equal(bankAccountsData[index]!.bankName);
+      expect(item.currentBalance).to.be.equal(bankAccountsData[index]!.currentBalance.toBRL());
+      expect(item.initialBalance).to.be.equal(bankAccountsData[index]!.initialBalance.toBRL());
     });
     expect(wallet.id).to.be.equal(walletData.id);
     expect(wallet.currentBalance).to.be.equal(walletData.currentBalance.toBRL());
-    expect(wallet.userId).to.be.equal(wallet.userId);
-  })
+    expect(wallet.userId).to.be.equal(walletData.userId);
+  });
 
   it("should not be able to get users account if the user does not exist in database", async () => {
     try {
-      await sut.execute({ userId: 'invalid-id' })
+      await sut.execute({ userId: "invalid-id" });
     } catch (error: any) {
       expect(error.message).to.be.equal("User not found");
       expect(error.statusCode).to.be.equal(404);
-      expect(error.errorType).to.be.equal("NOT_FOUND")
+      expect(error.errorType).to.be.equal("NOT_FOUND");
     }
-  })
+  });
 
   it("should not be able to get users account if the user does not have a wallet", async () => {
-    const userWithoutWallet = await createUser({})
-    inMemoryUsers.push(userWithoutWallet)
-    
+    const userWithoutWallet = await createUser({});
+    inMemoryUsers.push(userWithoutWallet);
+
     try {
-      await sut.execute({ userId: userWithoutWallet.id })
+      await sut.execute({ userId: userWithoutWallet.id });
     } catch (error: any) {
       expect(error.message).to.be.equal("Wallet not found");
       expect(error.statusCode).to.be.equal(404);
-      expect(error.errorType).to.be.equal("NOT_FOUND")
+      expect(error.errorType).to.be.equal("NOT_FOUND");
     }
-  })
+  });
 });
