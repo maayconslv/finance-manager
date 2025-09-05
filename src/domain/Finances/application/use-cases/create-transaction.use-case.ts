170c350bf6928de3e8e7c20f95d9585921096358
mayconslv <mayconslv.contato@gmail.com>
2025-09-05 02:15:20 -0300

    refactor: improving use-cases performance

--- src/domain/Finances/application/use-cases/create-transaction.use-case.ts
+++ src/domain/Finances/application/use-cases/create-transaction.use-case.ts
@@ -1,12 +1,20 @@
 import { Service } from "typedi";
-import { TransactionEntity, Type } from "../../enterprise";
-import { BankAccountRepository, TransactionRepository, UserRepository } from "@/infrastructure/database/prisma";
+import { CategoryEntity, TransactionEntity, Type } from "../../enterprise";
+import {
+  BankAccountRepository,
+  TransactionRepository,
+  UserRepository,
+  WalletRepository,
+} from "@/infrastructure/database/prisma";
 import { NotFoundError } from "@/domain/errors";
 import { Money, UniqueEntityId } from "@/core/object-values";
 import { TransactionModel } from "../model/finances.model";
 import { TransactionMapper } from "../mapper/finances.mapper";
 import { CategoryRepository } from "@/infrastructure/database/prisma/category.prisma.repository";
 import { UnauthorizedError } from "routing-controllers";
+import { InternalServerError } from "@/domain/errors/internal-server.error";
+import { WalletEntity } from "@/domain/Auth/enterprise/entities";
+import { BankAccountEntity } from "@/domain/Accounts/enterprise";
 
 export interface CreateTransactionUseCaseRequest {
   userId: string;
@@ -21,41 +29,56 @@ export interface CreateTransactionUseCaseRequest {
 export class CreateTransactionUseCase {
   constructor(
     private readonly transactionRepository: TransactionRepository,
-    private readonly userRepository: UserRepository,
     private readonly categoryRepository: CategoryRepository,
     private readonly bankAccountRepository: BankAccountRepository,
+    private readonly walletRepository: WalletRepository,
   ) {}
 
   async execute(data: CreateTransactionUseCaseRequest): Promise<TransactionModel> {
-    const user = await this.userRepository.findById(data.userId);
-    const category = await this.categoryRepository.findById(data.categoryId);
-    const bankAccount = await this.bankAccountRepository.findById(data.bankAccountId);
+    const [wallet, category, bankAccount] = await Promise.all([
+      this.walletRepository.findByUserId(data.userId),
+      this.categoryRepository.findById(data.categoryId),
+      this.bankAccountRepository.findByIdAndUser(data.bankAccountId, data.userId),
+    ]);
 
-    if (!user || !bankAccount || !category) {
-      throw new NotFoundError();
+    if (!bankAccount || !category || !wallet) {
+      throw new InternalServerError("We dont found the recourse that you searching. Please, contact support.");
     }
-
     if (bankAccount.isDisable) {
-      throw new UnauthorizedError();
+      throw new UnauthorizedError("You can't register a transaction in a disabled bank account.");
     }
 
-    const transaction = TransactionEntity.create({
+    const transaction = this.createTransaction(data, category);
+    this.updateBalances(wallet, bankAccount, transaction);
+
+    await Promise.all([
+      this.bankAccountRepository.save(bankAccount),
+      this.transactionRepository.save(transaction),
+      this.walletRepository.save(wallet),
+    ]);
+
+    return TransactionMapper.toModel(transaction);
+  }
+
+  private createTransaction(data: CreateTransactionUseCaseRequest, category: CategoryEntity): TransactionEntity {
+    return TransactionEntity.create({
       amount: new Money(data.amount),
       type: data.type,
       description: data.description,
       bankAccountId: new UniqueEntityId(data.bankAccountId),
       category,
     });
+  }
 
-    if (transaction.type == "income") {
-      bankAccount.currentBalance.increaseAmount(transaction.amount.getInCents());
+  private updateBalances(wallet: WalletEntity, bankAccount: BankAccountEntity, transaction: TransactionEntity): void {
+    const amountInCents = transaction.amount.getInCents();
+
+    if (transaction.type === Type.income) {
+      wallet.increaseAmountInCents = amountInCents;
+      bankAccount.increaseAmountInCents = amountInCents;
     } else {
-      bankAccount.currentBalance.decreaseAmount(transaction.amount.getInCents());
+      wallet.decreaseAmountInCents = amountInCents;
+      bankAccount.decreaseAmountInCents = amountInCents;
     }
-
-    await this.bankAccountRepository.save(bankAccount);
-    await this.transactionRepository.save(transaction);
-
-    return TransactionMapper.toModel(transaction);
   }
 }
